36d27aff8b7eddcee929d9686e50a73240b6b221
getmikyled <mikylemosquera@gmail.com>
2023-12-09 00:26:40 -0800

    Update UI & Started CreateWall Mode

--- Tools/Assets/_Tools/3DRoomConstructor/Editor/CreateWalls.cs
+++ Tools/Assets/_Tools/3DRoomConstructor/Editor/CreateWalls.cs
@@ -1,13 +1,108 @@
+using Codice.Client.Common.GameUI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public partial class CreateWalls
+namespace GetMikyled.RoomConstructor
 {
-    bool isCreatingWalls = false;
-
-    private void CreateButtonPressed()
+    public partial class RoomConstructor
     {
-        isCreatingWalls = true;
+        public bool isCreatingWall = false;
+
+        Transform wallObjectPool;
+        Transform wallStart;
+        Transform wallEnd;
+        Transform wall;
+
+        FloatField wallThickness;
+        FloatField wallHeight;
+
+        private void OnEnable()
+        {
+            SceneView.duringSceneGui += OnSceneGUI;
+            CreateWallObjectPool();
+        }
+
+        private void OnDisable()
+        {
+            DestroyWallObjectPool();
+        }
+
+        private void OnSceneGUI(SceneView sceneView)
+        {
+            if (currentState == BuildMode.CreateWalls)
+            {
+                Event eventCurrent = Event.current;
+                if (eventCurrent.button == 0)
+                {
+                    if (eventCurrent.type == EventType.MouseDown)
+                    {
+                        SetWallStart();
+                    }
+                    else if (eventCurrent.type == EventType.MouseUp)
+                    {
+                        SetWallEnd();
+                    }
+                    else
+                    {
+                        if (isCreatingWall)
+                        {
+                            AdjustWall();
+                        }
+                    }
+                }
+            } 
+        }
+
+        private void EnableCreatingWalls()
+        {
+            UpdateMode(BuildMode.CreateWalls, currentState);
+        }
+
+        private void SetWallStart()
+        {
+            isCreatingWall = true;
+        }
+
+        private void SetWallEnd()
+        {
+            isCreatingWall = false;
+        }
+
+        private void AdjustWall()
+        {
+                
+        }
+
+        private void CreateWallObjectPool()
+        {
+            wallObjectPool = new GameObject().transform;
+            wallObjectPool.name = "WallObjectPool";
+
+            wallStart = new GameObject().transform;
+            wallStart.name = "StartPoint";
+            wallEnd = new GameObject().transform;
+            wallEnd.name = "EndPoint";
+            wall = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+            wall.localScale = new Vector3(wall.localScale.x, float.Parse(wallHeight.text), float.Parse(wallThickness.text));
+            wall.name = "Wall";
+
+            wallStart.transform.parent = wallObjectPool;
+            wallEnd.transform.parent = wallObjectPool;
+            wall.parent = wallObjectPool;
+        }
+
+        private void DestroyWallObjectPool()
+        {
+            DestroyImmediate(wallObjectPool);
+            wallObjectPool = null;
+            wallStart = null;
+            wallEnd = null;
+            wall = null;
+        }
+
     }
 }
--- Tools/Assets/_Tools/3DRoomConstructor/Editor/RoomConstructor.cs
+++ Tools/Assets/_Tools/3DRoomConstructor/Editor/RoomConstructor.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace GetMikyled.RoomConstructor
 {
-    public enum 
-    public class RoomConstructor : EditorWindow
+    public enum BuildMode
     {
-        [SerializeField] private VisualTreeAsset _tree;
+        None = 0,
+        CreateWalls = 1
+    }
 
+    public partial class RoomConstructor : EditorWindow
+    {
+        BuildMode currentState = BuildMode.None;
+
+        #region CreateGUI
         /// <summary>
         /// CREATE THE WINDOW
         /// CLONE ELEMENTS
         /// </summary>
+        [SerializeField] private VisualTreeAsset _tree;
+
+        Label currentStateText;
+
         [MenuItem("Tools/RoomConstructor")]
         public static void ShowWindow()
         {
@@ -23,7 +35,25 @@ namespace GetMikyled.RoomConstructor
 
         private void CreateGUI()
         {
+            VisualElement root = this.rootVisualElement;
+
             _tree.CloneTree(rootVisualElement);
+
+            currentStateText = root.Q<Label>("CurrentMode");
+            UpdateMode(BuildMode.None, BuildMode.None);
+
+            Button createWallsButton = root.Q<Button>(name: "CreateWallsButton");
+            createWallsButton.clicked += () => EnableCreatingWalls();
         }
+        #endregion
+
+        #region CurrentStateLabel
+        private void UpdateMode(BuildMode newMode, BuildMode oldMode)
+        { 
+            currentState = newMode;
+            // MAKE IT UPDATE TO NAME OF ENUM
+            //currentStateText.text = "Current Mode: " + System.Enum.GetName((typeof(BuildModeState), currentState));
+        }
+        #endregion
     }
 }
